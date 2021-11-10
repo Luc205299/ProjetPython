de2121e25afd65123d14aebefec3293ba2b2becb
luc205299 <lucasbarthelemy43@gmail.com>
2021-11-10 19:17:55 +0100

    TKT

--- main.py
+++ main.py
@@ -4,3 +4,13 @@
 Pseudo=str(input("Rentrer votre Speudonyme : "))
 
 Genre = int(input("Si vous êtes un Homme tapez 1 ; \nSi vous êtes une Femme tapez 2 ; \nSi vous souhaitez ne pas rentrer votre genre, tapez 3 : "))
+
+match Genre:
+    case 1:
+        Genre = "Homme"
+    case 2 :
+        Genre = "Femme"
+    case 3 :
+        Genre = "Non Précisé"
+
+Age = int(input("Rentrer votre âge : "))
