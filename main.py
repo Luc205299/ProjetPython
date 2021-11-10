af48a7e1c96aab4ea30b9f9284f649c4ba34df70
luc205299 <lucasbarthelemy43@gmail.com>
2021-11-10 19:03:18 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+
+#Création du Profil
+
+Pseudo=str(input("Rentrer votre Speudonyme : "))
+
+Genre = int(input("Si vous êtes un Homme tapez 1 ; \nSi vous êtes une Femme tapez 2 ; \nSi vous souhaitez ne pas rentrer votre genre, tapez 3 : "))
