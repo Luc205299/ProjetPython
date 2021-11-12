4ba8c1f2712234199cce0bdffc9b595b5846243a
luc205299 <lucasbarthelemy43@gmail.com>
2021-11-12 20:32:32 +0100

    ajout d'une variable style pour demander le style que le lecteur prefere lors de la creation du profim

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ match Genre:
 # age : en dessous de 18 ans , 18-25 ans  et + de 25 ans
 Age = int(input("Rentrer votre âge : "))
 match Age:
-    case mineur if Age < 18:
+    case Age if Age < 18:
         print("Vous avez en dessous de 18 ans.")
-    case entre2 if Age >= 18 and Age <= 25 :
+    case Age if Age >= 18 and Age <= 25 :
         print("Vous avez entre 18 et 25 ans.")
-    case mineur if Age > 25 :
+    case Age if Age > 25 :
         print("Vous avez au dessus de 25 ans")
+
+Style = int(input("Rentrer 1 si vous aimer la Science-fiction,\nRentrer 2 si vous aimer la Biographie,\nRentrer 3 si vous aimer l'Horreur,\nRentrer 4 si vous aimer la Romance,\nRentrer 5 si vous aimer les Fables,\nRentrer 6 si vous aimer l'Histoire,\nRentrer 7 si vous aimer la Comédie : "))
