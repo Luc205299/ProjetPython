be3fcc3a4e8e819d72eef730c82492c411660e9a
luc205299 <lucasbarthelemy43@gmail.com>
2021-11-17 19:25:59 +0100

    test codewithmee

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from functionUsers import*
 
 #on demande si il veut se creer un profil, se connecter, rechercher un utilisateur ou quitter
-create=int(input("voulez vous vous connectez ? \n 1 pour créer un profil;\n 2 pour poursuivre a la connection;\n 3 pour rechercher un utilisateur;\n 4 pour quitter : "))
-while create > 4 or create < 1:
-    create=int(input("voulez vous vous connectez ? ( 1 pour créer un profil et 2 pour poursuivre a la connection : "))
-match create:
+Interface=int(input("voulez vous vous connectez ? \n 1 pour créer un profil;\n 2 pour poursuivre a la connection;\n 3 pour rechercher un utilisateur;\n 4 pour quitter : "))
+while Interface > 4 or Interface < 1:
+    Interface=int(input("voulez vous vous connectez ? ( 1 pour créer un profil et 2 pour poursuivre a la connection : "))
+match Interface:
     case 1:
-        create=1
+        Interface=1
     case 2:
-        create=2
+        Interface=2
     case 3:
-        create=3
+        Interface=3
     case 4:
-        create=4
-if create == 1:
+        Interface=4
+if Interface == 1:
     Profil("readers.txt")
-elif create ==2:
+elif Interface ==2:
     name = str(input("veuillez rentrer votre Speudo : "))
     Connection("readers.txt", name)
-elif create == 3:
+
+elif Interface == 3:
     namelook = str(input("veuillez rentrer le Speudo de la personne que vous cherchez : "))
     DisplayUsers("readers.txt", namelook)
 else:
