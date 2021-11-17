ff9a201e62c3b40de745312cff9c97fc9048d18b
luc205299 <lucasbarthelemy43@gmail.com>
2021-11-17 18:15:37 +0100

    Add Affichage , recherche utilisateur et mise en place d'une interface

--- function.py
+++ function.py
@@ -1,27 +1,66 @@
-def Profil() :
+def Profil(folder) :
+    with open (folder,"a") as readers:
 
-#Création du Profil
+    #Création du Profil
 
-    Pseudo=str(input("Rentrer votre Speudonyme : "))
+        Pseudo=str(input("Rentrer votre Pseudonyme : "))
+        readers.write(Pseudo+" ,")
+        Genre=0
+        while Genre <= 0 or Genre>4:
+            Genre = int(input("Si vous êtes un Homme tapez 1 ; \nSi vous êtes une Femme tapez 2 ; \nSi vous souhaitez ne pas rentrer votre genre, tapez 3 : "))
+            match Genre:
+                case 1:
+                    Genre = 1
+                case 2 :
+                    Genre = 2
+                case 3 :
+                    Genre = 3
 
-    Genre = int(input("Si vous êtes un Homme tapez 1 ; \nSi vous êtes une Femme tapez 2 ; \nSi vous souhaitez ne pas rentrer votre genre, tapez 3 : "))
 
-    match Genre:
-        case 1:
-            Genre = "Homme"
-        case 2 :
-            Genre = "Femme"
-        case 3 :
-            Genre = "Non Précisé"
+        readers.write(str(Genre)+" ,")
+        # age : en dessous de 18 ans , 18-25 ans  et + de 25 ans
+        Age = int(input("Rentrer votre âge : "))
+        match Age:
+            case Age if Age < 18:
+                print("Vous avez en dessous de 18 ans.")
+                Age = 1
+            case Age if Age >= 18 and Age <= 25 :
+                print("Vous avez entre 18 et 25 ans.")
+                Age= 2
+            case Age if Age > 25 :
+                print("Vous avez au dessus de 25 ans")
+                Age = 3
+        readers.write(str(Age)+" ,")
+        Style = int(input("Rentrer 1 si vous aimer la Science-fiction,\nRentrer 2 si vous aimer la Biographie,\nRentrer 3 si vous aimer l'Horreur,\nRentrer 4 si vous aimer la Romance,\nRentrer 5 si vous aimer les Fables,\nRentrer 6 si vous aimer l'Histoire,\nRentrer 7 si vous aimer la Comédie : "))
+        readers.write(str(Style)+"\n")
 
-    # age : en dessous de 18 ans , 18-25 ans  et + de 25 ans
-    Age = int(input("Rentrer votre âge : "))
-    match Age:
-        case Age if Age < 18:
-            print("Vous avez en dessous de 18 ans.")
-        case Age if Age >= 18 and Age <= 25 :
-            print("Vous avez entre 18 et 25 ans.")
-        case Age if Age > 25 :
-            print("Vous avez au dessus de 25 ans")
+    readers.close
 
-    Style = int(input("Rentrer 1 si vous aimer la Science-fiction,\nRentrer 2 si vous aimer la Biographie,\nRentrer 3 si vous aimer l'Horreur,\nRentrer 4 si vous aimer la Romance,\nRentrer 5 si vous aimer les Fables,\nRentrer 6 si vous aimer l'Histoire,\nRentrer 7 si vous aimer la Comédie : "))
+
+def Connection(folder,name):
+    with open(folder,"r") as readers:
+        verification=False
+        while verification==False:
+            line = readers.readline()
+            while line != "" and verification== False:
+                if name in line:
+                    verification=True
+                else:
+                    line=readers.readline()
+        print("bonjour ",name)
+    readers.close
+
+def DisplayUsers(folder, name):
+    with open(folder, "r") as readers:
+        verification = False
+        while verification == False:
+            line = readers.readline()
+            while line != "" and verification == False:
+                if name in line:
+                    verification = True
+                else:
+                    line = readers.readline()
+    readers.close
+    return(print(line))
+
+
--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 from function import*
-Profil()
+
+
+create=int(input("voulez vous vous connectez ? ( 1 pour créer un profil et 2 pour poursuivre a la connection : "))
+while create > 2 or create < 1:
+    create=int(input("voulez vous vous connectez ? ( 1 pour créer un profil et 2 pour poursuivre a la connection : "))
+match create:
+    case 1:
+        create=1
+    case 2:
+        create=2
+if create == 1:
+    Profil("readers.txt")
+
+else:
+    connection=int(input("voulez vous vous connectez ? si oui tapez 1. Si vous voulez rechercher un utilisateur, tapez 2"))
+    while connection<1 or connection>3:
+        connection = int(input("voulez vous vous connectez ? si oui tapez 1. Si vous voulez rechercher un utilisateur, tapez 2 ou tapez 3 pour quitter."))
+    match connection:
+        case 1:
+            connection=1
+        case 2:
+            connection=2
+        case 3:
+            connection=3
+    if connection == 1:
+        name = str(input("veuillez rentrer votre Speudo : "))
+        Connection("readers.txt",name)
+    elif connection == 2 :
+        namelook = str(input("veuillez rentrer le Speudo de la personne que vous cherchez : "))
+        DisplayUsers("readers.txt", namelook)
+    else:
+        exit()
