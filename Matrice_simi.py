9fe8b1106180aa54dde3fe504c0e1e6a4fbf0a4d
SylvaBHD2 <93947829+SylvaBHD2@users.noreply.github.com>
2021-12-16 20:45:05 +0100

    Function Matrix simi finie a optimiser et tester

--- Matrice_simi.py
+++ Matrice_simi.py
@@ -12,15 +12,15 @@ def init_matrixSimi(file, file2):
     file : readers.txt
     file 2: matrix"""
     global Matrix_simi
-    Matrix1 = import_matrix(file2)
+    matrix1 = import_matrix(file2)
     # transform each str note to int an int value
-    for line in range(1, len(Matrix1)):
-        for row in range(1, len(Matrix1[line])):
-            Matrix1[line][row] = int(Matrix1[line][row])
+    for line in range(1, len(matrix1)):
+        for row in range(1, len(matrix1[line])):
+            matrix1[line][row] = int(matrix1[line][row])
     # add readers list as first line
-    Matrix_simi.append([Matrix1[i][0] for i in range(len(Matrix1))])
+    Matrix_simi.append([matrix1[i][0] for i in range(len(matrix1))])
     # append each reader to the head of each line initialized with 0
-    for i in range(1, len(Matrix1)):
+    for i in range(1, len(matrix1)):
         tmp = [j - j for j in range(len(Matrix_simi[0]))]
         tmp[0] = Matrix_simi[0][i]
         Matrix_simi.append(tmp)
@@ -36,8 +36,8 @@ def init_matrixSimi(file, file2):
             else:
                 # two compared lines, without the pseudo
                 sum1, sum2 = 0, 0
-                a = Matrix1[i][1:]
-                b = Matrix1[j][1:]
+                a = matrix1[i][1:]
+                b = matrix1[j][1:]
                 # compare each value of the line with every other of the same column
                 for k in range(1, len(a)):
                     sum1 += a[k] ** 2
@@ -56,10 +56,7 @@ def init_matrixSimi(file, file2):
     save_matrix(file, Matrix_simi)
 
 
-# init_matrixSimi("Matrice_similarité.txt", "Matrix.txt")
-
-
-def book_suggestion(file1, file2, file3, file4, name):
+def book_suggestion(file1, file2, file3, file4, name: str):
     """suggest a book nased on the matrix_simi
     -file1 : readers.txt
     -file2 : Matrice_similarité.txt
@@ -74,7 +71,6 @@ def book_suggestion(file1, file2, file3, file4, name):
     #     return print("Veuillez vous inscrire d'abord")
 
     matrix = import_matrix(file2)
-    print("test", matrix)
     # transform each str note to int an int value
     for line in range(1, len(matrix)):
         for row in range(1, len(matrix[line])):
@@ -83,53 +79,50 @@ def book_suggestion(file1, file2, file3, file4, name):
     # search for the line of the user
     for line in range(1, len(matrix)):  # remplacer par un while
         if matrix[line][0] == name:
-            print(f"trouvé !, line = {matrix[line]}")
             user_line = matrix[line]
 
     # deduce the person the most similar to the given name byb grabbing the number of column
-    sibling_index = user_line.index(max([0 if elt == 1 else elt for elt in user_line[1:]])) # a optimiser
-    sibling = matrix[0][sibling_index]
-    print("trouvé sibling = ", sibling)
+    sibling = matrix[0][user_line.index(max([0 if elt == 1 else elt for elt in user_line[1:]]))]
 
     # show books that the sibling has readen
-    with open(file3, "r", encoding = 'utf-8') as bksr:
+    with open(file3, "r", encoding='utf-8') as bksr:
         # list of every line in booksread
-        line = bksr.readlines()
-        print("line =", line)
-        # list of every line numbe rof books
+        line_bksr = [elt.strip('\n').split(",") for elt in bksr.readlines()]
+        # list of every line number of books in the sibling profile
         line_number = []
-        # search the line with the same noun as the sibling in evrey line
-        for elt in line:
+        # search the line with the same noun as the sibling in every line
+        for elements in line_bksr:
             # elements is th list of reader one by one
-            elements = elt.strip('\n').split(",")
-            print("elements", elements)
-            print(elements[0], " == ?", sibling)
             if elements[0] == sibling:
-                line_number = [int(elt) for elt in elements[1:]]
-                print("test", elements, '\n', line_number)
+                linebksr_sibling = elements
+            elif elements[0] == name:
+                linebksr_user = elements
+
+        # add every book that hasn't been readen yet by their index
+        for elt in linebksr_sibling[1:]:
+            if elt not in linebksr_user[1:]:
+                line_number.append(int(elt))
+
         # go through every book to add it
         with open(file4, "r", encoding='utf-8') as bks:
-            # list of read books by the sibling presnted at the end
-            line_bks = []
             # list of all books
             listebook = [elt.strip('\n') for elt in bks.readlines()]
+            # transform every book index into the title
+            line_bks = [listebook[elt] for elt in line_number]
+        if len(line_bks) == 0:
+            print("Malheureusement, aucun livre ne peut vous etre suggerer, vous etes clui qui a le plus lu de livres")
+        else:
+            print("Voici le(s) livre(s) susceptibles de vous plaire: ")
+            print(*line_bks)
 
-            print("try", listebook, '\n', elements)
-            # for each number in element
-            for elt in line_number:
-                line_bks.append(listebook[elt])
-        print(" voici les livres qui son susceptibles de vous plaire; ")
-        print(line_bks)
-        # imprime la liste des livres
     # imput the concerned book
     chc = str(input("Choissisez en un : "))
     while chc not in line_bks: # a changer avec l'index !!!
         chc = str(input("Veuillez en choisir un parmi tous ceux là :"))
         print(line_bks)
-    #print("chc", chc)
     # after the user chose a book
     booksread_addBook(file1, file4, file3, name, chc)
-    answer = str(input("voulez vous le noter? (si vous l'avez déja lu) ( y / n )"))
+    answer = str(input("voulez vous le noter? ( y / n ):"))
     if answer == "y":
         fm.update_Matrix(file4, file3, name, chc)
     else:
