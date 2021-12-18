e22594518226ac56fb682eaa60516c15bc4eff87
luc205299 <lucasbarthelemy43>
2021-12-18 19:19:39 +0100

    propre

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ text_Userinterface="What would you like to do ? \n 1 to enter the books you have
                  "account;\n 4 to get a book " \
                  "suggestion; \n 5 to see every book available ;\n 6 to look for a particular book ;\n 7 to add a " \
                  "book to our database ;\n 8 to check if you already read a specific book ; \n 9 to log out :"
+
 text_Admininterface = "What would you like to do ? \n 1 to verify that the account exists;\n 2 to change the title of a book;\n 3 to delete a book from the database " \
                  ";\n 4 to quit ; \n :"
 # menu for the user, making him able to interact with the program
@@ -27,87 +28,77 @@ while Interface < 0 or Interface > 4 or Interface is None:
         input(text_interface))
 
 # initialise the pseudo as an empty str
-pseudo = ""
 
-while Interface < 0 or Interface > 4 or Interface is not None:
+match Interface:
+    case 1:
+        Profil("readers.txt", "booksread.txt", Matrix)
 
-    match Interface:
-        case 1:
-            Profil("readers.txt", "booksread.txt", Matrix)
+    case 2:
+        pseudo_temp = str(input("Please enter your pseudo : "))
+        pseudo = Connection("readers.txt", pseudo_temp)
+        if pseudo!= "":
+            userinterface=(int(
+input(text_Userinterface)))
+            while userinterface < 16 or userinterface > 1 or userinterface is None:
+                userinterface = int(
+                input(text_Userinterface))
+            match userinterface:
 
-        case 2:
-            pseudo_temp = str(input("Please enter your pseudo : "))
-            pseudo = Connection("readers.txt", pseudo_temp)
-            if pseudo!= "":
-                userinterface=(int(
-    input(text_Userinterface)))
-                while userinterface < 16 or Interface > 1 or Interface is None:
-                    userinterface = int(
-                    input(text_Userinterface))
-                match userinterface:
-
-                    case 1:
-                        booksread_addBook("readers.txt", "books.txt", "booksread.txt", pseudo)
-                    case 2:
-                        update_Matrix("books.txt", "booksread.txt", pseudo)
-                    case 3:
-                        answer = str(input("Are you sure you want to delete your profile ? (yes / no) : "))
-                        while answer != "yes" or answer != "no":
-                            answer = str(input("Are you sure you want to delete your profile ? (yes / no) : "))
-                        match answer:
-                            case "yes":
-                                DeleteUsers("readers.txt", "booksread.txt", pseudo)
-                            case "no":
-                                Interface = 100
-                    case 4:
-                        init_matrixSimi("readers.txt", "matrix.txt")
-                        book_suggestion("readers.txt", "matrix_simi.txt", "booksread.txt", "books.txt", pseudo)
-                    case 5:
-                        ShowBook("books.txt")
-                    case 6:
-                        search = book_exist("books.txt", str(input("What is the book title you are looking for?")))
-                        print("book is already in the database ." if search == True else "You can add this book .")
-                        search = ""
-                    case 7:
-                        addBook("books.txt", "matrix.txt", str(input("What is the title of the new book ?")))
-
-                    case 8:
-                        if pseudo == "":
-                            print("Please, connect to your account.")
-                        else:
-                            booksread_verify("books.txt", "booksread.txt", pseudo,
-                                             str(input("What is the title of the book that "
-                                                       "you need to check ?")))
-                    case 9:
-                        #log out
-                        name = ""
-                        print("successfully logged out")
-
-        case 3:
-            search_ps = str(input("What is the pseudo of the person you are looking for ? : "))
-            DisplayUsers("readers.txt", "booksread.txt", search_ps)
-
-        case 4 :
-            admininterface=(int(
-    input(text_Admininterface)))
-            while 1>admininterface or admininterface > 4:
-                admininterface = (int(
-                    input(text_Admininterface)))
-            match admininterface:
                 case 1:
-                    print(users_exist("readers.txt", pseudo))
-                case 4:
-                    exit()
+                    booksread_addBook("readers.txt", "books.txt", "booksread.txt", pseudo)
                 case 2:
-                    changeTitle("books.txt")
+                    update_Matrix("books.txt", "booksread.txt", pseudo)
                 case 3:
-                    delete_Book("books.txt", "booksread.txt", "matrix.txt")
+                    answer = str(input("Are you sure you want to delete your profile ? (yes / no) : "))
+                    while answer != "yes" or answer != "no":
+                        answer = str(input("Are you sure you want to delete your profile ? (yes / no) : "))
+                    match answer:
+                        case "yes":
+                            DeleteUsers("readers.txt", "booksread.txt", pseudo)
+                        case "no":
+                            Interface = 100
+                case 4:
+                    init_matrixSimi("readers.txt", "matrix.txt")
+                    book_suggestion("readers.txt", "matrix_simi.txt", "booksread.txt", "books.txt", pseudo)
+                case 5:
+                    ShowBook("books.txt")
+                case 6:
+                    search = book_exist("books.txt", str(input("What is the book title you are looking for?")))
+                    print("book is already in the database ." if search == True else "You can add this book .")
+                    search = ""
+                case 7:
+                    addBook("books.txt", "matrix.txt", str(input("What is the title of the new book ?")))
 
-
-
-
-
-
+                case 8:
+                    if pseudo == "":
+                        print("Please, connect to your account.")
+                    else:
+                        booksread_verify("books.txt", "booksread.txt", pseudo,
+                                         str(input("What is the title of the book that "
+                                                   "you need to check ?")))
+                case 9:
+                    #log out
+                    name = ""
+                    print("successfully logged out")
+    case 3:
+        search_ps = str(input("What is the pseudo of the person you are looking for ? : "))
+        DisplayUsers("readers.txt", "booksread.txt", search_ps)
+    case 4 :
+        admininterface=(int(
+input(text_Admininterface)))
+        while 1>admininterface or admininterface > 4:
+            admininterface = (int(
+                input(text_Admininterface)))
+        match admininterface:
+            case 1:
+                user=str(input("Enter the username of the user you looking for : "))
+                print(users_exist("readers.txt", user))
+            case 4:
+                exit()
+            case 2:
+                changeTitle("books.txt")
+            case 3:
+                delete_Book("books.txt", "booksread.txt", "matrix.txt")
 
 # keep user in the  while loop
 #Interface = 100
